aeebe6813c8bf32e1738110b609810779de0fd3d
Alucardik <ichekk@yandex.ru>
2021-04-10 04:05:11 +0300

    feat: added csv conversion tool and binded spreadsheets API

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,62 @@ import CalendarHeader from "@/components/CalendarHeader";
 import SimpleCalendarView from "@/components/SimpleCalendarView";
 import AdvancedCalendarView from "@/components/AdvancedCalendarView";
 import shared from "@/assets/scripts/utils/shared";
-import consts from "@/assets/scripts/utils/constants";
+import constants from "@/assets/scripts/utils/constants";
+import Pizzly from 'pizzly-js';
 
 export default {
   name: 'App',
 
   created() {
-    this.months = consts.months;
+    const json2csv = require('csvjson-json2csv');
+    const csv = json2csv(constants.obj_test);
+    console.log(csv);
+    // console.log(JSON.stringify(constants.obj_test));
+    // console.log(JSON.stringify(csv));
+
+    //______________________________________________________________
+    // const demoInstance = "https://psyc-calendar.herokuapp.com/";
+    // const pubKey = "pope8Qy8qfYyppnHRMgLMpQ8MuEUKDGeyhfGCj";
+    //______________________________________________________________
+
+    const pizzly = new Pizzly({
+      host: "https://psyc-calendar.herokuapp.com/",
+      publishableKey: "pope8Qy8qfYyppnHRMgLMpQ8MuEUKDGeyhfGCj"
+    });
+    const spreadsheetId = '1Ruh0BsRYzwbePVC8SczTSGxLRlNQtCyCMZn7ez0W14U';
+
+    pizzly
+      .integration("google-sheets")
+      .auth("20d47430-9980-11eb-849a-795b78c342fc")
+      .post(`${spreadsheetId}:batchUpdate`,{
+        body: JSON.stringify({
+          "requests": [
+            {
+              "pasteData": {
+                "coordinate": {
+                  "sheetId": 0,
+                  "rowIndex": 0,
+                  "columnIndex": 0
+                },
+                "data": csv,
+                "type": "PASTE_VALUES",
+                "delimiter": ",",
+              }
+            }
+          ],
+          "includeSpreadsheetInResponse": false,
+          "responseRanges": [],
+          "responseIncludeGridData": false
+        })
+      })
+      .then((response) => {
+        console.log(response.status);
+        return response.json();
+        // return response.body;
+      })
+      .then((res) => {
+        console.log(res);
+      });
   },
 
   data: function() {
