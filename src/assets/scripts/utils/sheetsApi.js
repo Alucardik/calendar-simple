bf27ffb49b414ac2bc49443961c614981497b8ab
Alucardik <ichekk@yandex.ru>
2021-06-03 15:10:44 +0300

    upd authId

--- src/assets/scripts/utils/sheetsApi.js
+++ src/assets/scripts/utils/sheetsApi.js
@@ -5,7 +5,7 @@ const pizzly = new Pizzly({
   publishableKey: "pope8Qy8qfYyppnHRMgLMpQ8MuEUKDGeyhfGCj"
 });
 const spreadsheetId = '1Ruh0BsRYzwbePVC8SczTSGxLRlNQtCyCMZn7ez0W14U';
-const authId = "f9580380-be2e-11eb-8bd0-9ff158aa406e";
+const authId = "609dcc60-c464-11eb-bdbb-b3a475537fb7";
 
 function sendStats(csvData, sheetName) {
   return pizzly.integration("google-sheets")
